d96d7fc704a639cd8683c09f3ff34776b220d553
aldo1411 <62414829+aldo1411@users.noreply.github.com>
2022-09-01 19:41:31 -0500

    commit

---
+++ index.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+datos = pd.read_csv('./manejoDeDatos/data.csv')
+
+
+#top 10 canciones bailables
+def top10() :
+  print('top 10 canciones bailables')
+  print('---------------------------------------------------------------------------')
+  print(pd.DataFrame(datos).sort_values('danceability').head(10))
+  print('---------------------------------------------------------------------------')
+  pd.DataFrame(datos['danceability']).head(10).plot()
+  plt.show()
+
+#cómo se ven los datos 'danceability', 'energy', 'key', 'loudness' de las top 10 canciones likeadas
+def datosLiked() :
+  df2 = pd.DataFrame(datos, columns=['liked', 'danceability', 'energy', 'key', 'loudness'])
+  print('top 10 canciones bailables')
+  print('---------------------------------------------------------------------------')
+  print(df2.loc[df2['liked'] == 1].head(10))
+  print('---------------------------------------------------------------------------')
+  pd.DataFrame(df2.loc[df2['liked'] == 1].head(10)).plot()
+  plt.show()
+
+#valor de energy más alto
+def datosEnergy() :
+  print('---------------------------------------------------------------------------')
+  print(pd.DataFrame(datos)['energy'].max())
+  print('---------------------------------------------------------------------------')
+  pd.DataFrame(datos)['energy'].plot(kind='hist')
+  plt.show()
+
+#top10()
+#datosLiked()
+datosEnergy()
